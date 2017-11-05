44af627e887068a3dd6ef2a7b7bb0cc8f283ab1b
okabi <okabi13@yahoo.co.jp>
2017-11-05 17:53:34 +0900

    Rails -> Python の Python ファイルの更新

--- main_save.py
+++ main_save.py
@@ -7,11 +7,15 @@ from color_util import ColorUtil
 from manager import Manager
 
 if __name__ == '__main__':
-    db = 'web/db/production.sqlite3'
+    db = '/home/okabi/Projects/mosaic/web/db/production.sqlite3'
     args = sys.argv
 
     manager = Manager(db)
-    infos = manager.get(id=args[1])
-    path = "web/public/images/{:}".format(infos[1])
-    h, s, v = ColorUtil.mean_hsv(cv2.imread(path))
+    infos = manager.get(id=args[1])[0]
+    path = "/home/okabi/Projects/mosaic/web/public/images/{:}".format(infos[1])
+    print path
+    image = cv2.imread(path)
+    print image
+    h, s, v = ColorUtil.mean_hsv(image)
+    print [h, s, v]
     manager.update_hsv(args[1], h, s, v)
